cc57cf5c5841463911ac4504ab2202643a05f65d
terraclime <hari@terraclime.com>
2026-02-11 18:54:24 +0530

    addada

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
+  process.env.REACT_APP_API_BASE_URL || "http://18.214.70.132:8080/api";
 
 const apiClient = axios.create({
   baseURL,
